8d690ed8d055b8b03bf637f6b374cbad67eca3af
Adem-hub <57374875+Adem-hub@users.noreply.github.com>
2020-05-07 15:04:23 +0200

    Add files via upload

--- SousListesDejaFaites.py
+++ SousListesDejaFaites.py
@@ -1,26 +1,7 @@
 ListesEleves=[["Adem","Othman","Jean"],["Baptiste","Jb","Jean","Adem"],["Alex","Gibaud","pzlce","Kevin"],["Gibaud","JeanBaptiste","Alex","Nouredaine","Jb"],["Gibaud","Baptiste"],["Othman","Adem","Noureddine"],["Noureddine","Jb","Baptiste","Jean"]]
 
-TousLesEleves=["Othman","Aya","Mehdi","Nour","Adem","Yanis","Alex","Noureddine","Alexandre","Aminata","Clément","Dayna","Fatima","Ismaël","Jeras","Kevindra","Nolan","Ruben","Rémi","Sidy","Tom","Walid","Yassine","Youcef","Younes","Younouss Soussi"]
 
 
-"""Cette fonction peut être utilisée à la place de ListesEleves dans for j in ListeEleves à la ligne 33 . Faudra faire un truc comme ListeTous=Random(), puis templacer la ligne 33 par for j in ListeTous, à ce moment, il faudra aussi remplacer ListeBase= Algo() l.30 par ListeBase= TousLesEleves
-"""
-
-def Random():
-    Eleves=["Othman","Aya","Mehdi","Nour","Adem","Yanis","Alex","Noureddine","Alexandre","Aminata","Clément","Dayna","Fatima","Ismaël","Jeras","Kevindra","Nolan","Ruben","Rémi","Sidy","Tom","Walid","Yassine","Youcef","Younes","Younouss Soussi"]
-    L=[]
-    for i in range(rd.randint(5,30)):
-        k=rd.randint(1,len(Eleves)-1)
-        L.append( [0,]*k)
-    for j in range (len(L)):
-        for l in range (len(L[j])):
-            a=rd.randint(0,len(Eleves)-1)
-            L[j][l]=Eleves[a]
-            if Eleves[a] in L[j]:
-                while L[j].count(Eleves[a])>1:
-                    a=rd.randint(0,len(Eleves)-1)
-                    L[j][l]=Eleves[a]
-    return L
 
 def Algo():
     L=[]
